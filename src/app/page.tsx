b44e22909dd1cf25497de66d70ee4af863d4e90b
Lucie Monteir <lucie.mtro@gmail.com>
2025-04-18 15:57:19 +0200

    fix: Adjust tortueland class positioning and ensure background image does not repeat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div
       className="relative flexjustify-center min-h-screen gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] 
-           bg-[url('/images/tortueland.jpeg')] bg-cover bg-no-repeat bg-center"
+           bg-[url('/images/tortueland.jpeg')] bg-cover bg-no-repeat bg-center tortueland"
     >
       <h1
         className="uppercase text-5xl sm:text-7xl md:text-8xl font-extrabold text-white text-center 
